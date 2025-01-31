397e95e0f40553b73b0d95ae9e605e5f5d62e558
vakarmak <vakarmak@gmail.com>
2025-01-31 16:10:30 +0200

    test changes

--- SeleniumCourse/Program.cs
+++ SeleniumCourse/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("New line");
+        Console.WriteLine("Hello, World!");
     }
 }
