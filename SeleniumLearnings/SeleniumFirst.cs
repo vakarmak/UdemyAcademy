a47a6468c4c65fb37f885beb219adbd787de48a4
vakarmak <vakarmak@gmail.com>
2025-02-12 00:48:33 +0200

    chrome driver config with simple test

--- SeleniumLearnings/SeleniumFirst.cs
+++ SeleniumLearnings/SeleniumFirst.cs
@@ -8,18 +8,26 @@ namespace SeleniumLearnings;
 public class SeleniumFirst
 {
     private IWebDriver _driver;
-    
+
     [SetUp]
     public void StartBrowser()
     {
         // ChromeDriver is a class that implements IWebDriver interface to interact with the Chrome browser.
         new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
         _driver = new ChromeDriver();
+        _driver.Manage().Window.Maximize();
     }
 
     [Test]
     public void Test1()
     {
-        _driver.Url = "https://www.google.com";
+        _driver.Url = "https://www.demoblaze.com/";
+        TestContext.Progress.WriteLine(_driver.Title);
+        TestContext.Progress.WriteLine(_driver.Url);
+        // TestContext.Progress.WriteLine is used to write to the test output.
+        _driver.Close();
+        //_driver.Quit();
+        // The difference between Close and Quit is that Close closes the current window whereas Quit closes all the windows.
+        
     }
 }
