b2ab97933e58b16d311b1a172a2b4e366955bb78
vakarmak <vakarmak@gmail.com>
2025-07-21 23:13:10 +0300

    small changes to avoid Warnings

--- CSharpFunds/CSharp/DataTypesFunds.cs
+++ CSharpFunds/CSharp/DataTypesFunds.cs
@@ -20,37 +20,37 @@ class Program : Inheritance
         Console.WriteLine("I'm inside the method");
     }
     
-    private static void Main()
-    {
-        Console.WriteLine("Data Types Fundamentals");
-        int a = 4;
-        Console.WriteLine("Value of a: " + a);
-        
-        String name = "Selenium";
-        Console.WriteLine("Value of name: " + name);
-        Console.WriteLine("------------------------");
-        
-        Console.WriteLine($"Value of name: {name}");
-        Console.WriteLine("------------------------");
-        
-        var age = 30;
-        Console.WriteLine("Age is: " + age);
-        // age = "Hello"; // Error
-        
-        Console.WriteLine("------------------------");
-        
-        // Dynamic Data Type gives flexibility to change the data type
-        dynamic height = 13.2;
-        Console.WriteLine("Height is: " + height);
-        height = "Hello"; // No Error
-        Console.WriteLine("Height is: " + height);
-        Console.WriteLine("------------------------");
-        
-        // Call a method from another class
-        Program p = new Program("Selenium");
-        p.GetData();
-        // Call a method from the inherited class
-        p.SetData();
-        p.GetName();
-    }
+    // private static void Main()
+    // {
+    //     Console.WriteLine("Data Types Fundamentals");
+    //     int a = 4;
+    //     Console.WriteLine("Value of a: " + a);
+    //     
+    //     String name = "Selenium";
+    //     Console.WriteLine("Value of name: " + name);
+    //     Console.WriteLine("------------------------");
+    //     
+    //     Console.WriteLine($"Value of name: {name}");
+    //     Console.WriteLine("------------------------");
+    //     
+    //     var age = 30;
+    //     Console.WriteLine("Age is: " + age);
+    //     // age = "Hello"; // Error
+    //     
+    //     Console.WriteLine("------------------------");
+    //     
+    //     // Dynamic Data Type gives flexibility to change the data type
+    //     dynamic height = 13.2;
+    //     Console.WriteLine("Height is: " + height);
+    //     height = "Hello"; // No Error
+    //     Console.WriteLine("Height is: " + height);
+    //     Console.WriteLine("------------------------");
+    //     
+    //     // Call a method from another class
+    //     Program p = new Program("Selenium");
+    //     p.GetData();
+    //     // Call a method from the inherited class
+    //     p.SetData();
+    //     p.GetName();
+    // }
 }
