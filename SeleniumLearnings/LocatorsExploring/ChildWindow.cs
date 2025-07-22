3bfd6f81ce622101f03a8567b974b7359e535311
vakarmak <vakarmak@gmail.com>
2025-07-22 22:14:02 +0300

    Windows handler part 1

---
+++ SeleniumLearnings/LocatorsExploring/ChildWindow.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumLearnings.Base;
+
+namespace SeleniumLearnings.LocatorsExploring;
+
+public class ChildWindow : UiTestFixture
+{
+    [Test]
+    public void ChildWindowTest()
+    {
+        NavigateTo("https://rahulshettyacademy.com/AutomationPractice/");
+
+        var newTabButton = Driver.FindElement(By.Id("opentab"));
+        newTabButton.Click();
+        
+        var tabsHandler = Driver.WindowHandles;
+        tabsHandler.Should().HaveCount(2, "There should be two tabs opened after clicking the 'newTabButton' button.");
+        
+        var childTab = tabsHandler[1];
+        Driver.SwitchTo().Window(childTab);
+        
+        var childTabTitle = Driver.FindElement(By.XPath("//img[@alt='Logo']"));
+        Wait.Until(d => childTabTitle.Displayed);
+        childTabTitle.Displayed.Should().BeTrue();
+    }
+}
