3e50826422663ca62a531a28f26d9eaeffff80cc
vakarmak <vakarmak@gmail.com>
2025-07-22 21:06:22 +0300

    Frame simple navigation

---
+++ SeleniumLearnings/LocatorsExploring/FrameExploring.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumLearnings.Base;
+
+namespace SeleniumLearnings.LocatorsExploring;
+
+public class FrameExploring : UiTestFixture
+{
+    [Test]
+    public void FrameExploringTest()
+    {
+        NavigateTo("https://rahulshettyacademy.com/AutomationPractice/");
+
+        var frameElement = Driver.FindElement(By.Id("courses-iframe"));
+        
+        Js.ExecuteScript("arguments[0].scrollIntoView(true);", frameElement);
+        
+        var mainFrame = Driver.SwitchTo().Frame("courses-iframe");
+        var allAccessPlanButton = mainFrame.FindElement(By.XPath("//a[text()='All Access plan']"));
+        
+        allAccessPlanButton.Click();
+    }
+}
