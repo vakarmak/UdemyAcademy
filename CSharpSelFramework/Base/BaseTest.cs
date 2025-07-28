ae844bbec455e289b0ebcf06581a5d3c117e1b33
Maksym Vakarchuk <vakarmak@gmail.com>
2025-07-28 13:08:04 +0300

    1. Added global configuration(browser init); 2. Make changes to csproj file to locate App.config file

--- CSharpSelFramework/Base/BaseTest.cs
+++ CSharpSelFramework/Base/BaseTest.cs
@@ -1,8 +1,11 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using System.Configuration;
 using WebDriverManager.DriverConfigs.Impl;
 
 namespace CSharpSelFramework.Base;
@@ -17,13 +20,36 @@ public class BaseTest
     [SetUp]
     public void Setup()
     {
-        new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-        Driver = new ChromeDriver();
+        //new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+        //Driver = new ChromeDriver();
+
+        var browserName = ConfigurationManager.AppSettings["browser"];
+        InitBrowser(browserName);
+
         Driver.Manage().Window.Maximize();
         
         Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
         Actions = new Actions(Driver);
         Js = (IJavaScriptExecutor)Driver;
+    }
+
+    protected void InitBrowser(string browserName)
+    {
+        switch (browserName)
+        {
+            case "Chrome":
+                new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                Driver = new ChromeDriver();
+                break;
+            case "Firefox":
+                new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+                Driver = new FirefoxDriver();
+                break;
+            case "Edge":
+                new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+                Driver = new EdgeDriver();
+                break;
+        }
     }
     
     protected void NavigateTo(string url)
