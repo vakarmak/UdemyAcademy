67c915682f8d8939e7cd0b774a2499962002380d
Maksym Vakarchuk <vakarmak@gmail.com>
2025-08-02 01:14:23 +0300

    ...

--- CSharpSelFramework/Utilities/BaseTest.cs
+++ CSharpSelFramework/Utilities/BaseTest.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using WebDriverManager.DriverConfigs.Impl;
